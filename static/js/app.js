4a418c63b3cb71bea262b204e27a5734f9b45915
abasha2002 <abashishvili2002@gmail.com>
2023-07-07 01:14:32 +0400

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ const CardCVC = document.getElementById("CVC");
 const CardNumberError = document.getElementById("number-error");
 const CardNameError = document.getElementById("name-error");
 const CardDateError = document.getElementById("date-error");
+const CardCVCError = document.getElementById("CVC-error");
 
 
 CardNumber.addEventListener('input', function(event) {
@@ -16,74 +17,108 @@ CardNumber.addEventListener('input', function(event) {
   const truncatedValue = formattedValue.slice(0, 16);
   const spacedValue = truncatedValue.replace(/(.{4})(?!$)/g, '$1 ');
   event.target.value = spacedValue;
+
+  const mastercardNumbers = /^(?:\d{4}\s){3}\d{4}$/;
+  if (!mastercardNumbers.test(CardNumber.value)) {
+    CardNumberError.textContent = 'Enter 16 digits';
+    CardNumber.style.borderColor = 'red';
+  }else{
+    CardNumberError.textContent = '';
+    CardNumber.style.borderColor = '';
+  }
+});
+
+CardName.addEventListener('input', function(event) {
+  const { value } = event.target;
+  const formattedValue = value.replace(/[0-9]/g, '');
+
+  if (formattedValue !== value) {
+    event.target.value = formattedValue;
+  }
+
+  if (/[0-9]/.test(value)) {
+    CardNameError.textContent = 'Digits are not allowed! Only letters';
+    CardName.style.borderColor = 'red';
+  } else {
+    CardNameError.textContent = '';
+    CardName.style.borderColor = '';
+  }
 });
 
 CardDate.addEventListener('input', function(event) {
   const { value } = event.target;
   const formattedValue = value.replace(/\D/g, '').slice(0, 2);
   event.target.value = formattedValue;
+
+  const isValid = /^\d{2}$/.test(CardDate.value) && /^\d{2}$/.test(CardDate1.value);
+  if (!isValid) {
+    CardDateError.textContent = 'Enter 2 digits';
+    CardDate.style.borderColor = 'red';
+    CardDate1.style.borderColor = 'red';
+  } else {
+    CardDateError.textContent = '';
+    CardDate.style.borderColor = '';
+    CardDate1.style.borderColor = '';
+  }
 });
 
 CardDate1.addEventListener('input', function(event) {
   const { value } = event.target;
   const formattedValue = value.replace(/\D/g, '').slice(0, 2);
   event.target.value = formattedValue;
+
+  const isValid = /^\d{2}$/.test(CardDate.value) && /^\d{2}$/.test(CardDate1.value);
+  if (!isValid) {
+    CardDateError.textContent = 'Enter 2 digits';
+    CardDate.style.borderColor = 'red';
+    CardDate1.style.borderColor = 'red';
+  } else {
+    CardDateError.textContent = '';
+    CardDate.style.borderColor = '';
+    CardDate1.style.borderColor = '';
+  }
 });
 
 CardCVC.addEventListener('input', function(event) {
   const { value } = event.target;
   const formattedValue = value.replace(/\D/g, '').slice(0, 3);
   event.target.value = formattedValue;
+
+  if (!/^\d{3}$/.test(CardCVC.value)) {
+    CardCVCError.textContent = 'Enter 3 digits';
+    CardCVC.style.borderColor = 'red';
+  }else{
+    CardCVCError.textContent = '';
+    CardCVC.style.borderColor = '';
+  }
+
 });
 
 
 CardForm.addEventListener('submit', function(event) {
   let isValid = true;
 
-  CardNumberError.textContent = '';
   CardNameError.textContent = '';
-  CardDateError.textContent = '';
 
   const mastercardNumbers = /^(?:\d{4}\s){3}\d{4}$/;
   if (!mastercardNumbers.test(CardNumber.value)) {
-    CardNumberError.textContent = 'Woops! Wrong format, fill the input please.';
-    CardNumber.style.borderColor = 'red';
     isValid = false;
-  }else{
-    CardNumber.style.borderColor = '';
   }
 
   if (!/^[A-Za-z\s]+$/.test(CardName.value)) {
-    CardNameError.textContent = 'Woops! Wrong format, letters only.';
-    CardName.style.borderColor = 'red';
     isValid = false;
-  } else {
-    CardName.style.borderColor = '';
   }
 
-
   if (!/^\d{2}$/.test(CardDate.value)) {
-    CardDateError.textContent = 'Woops! Wrong format, fill the input please.';
-    CardDate.style.borderColor = 'red';
     isValid = false;
-  }else{
-    CardDate.style.borderColor = '';
   }
 
   if (!/^\d{2}$/.test(CardDate1.value)) {
-    CardDateError.textContent = 'Woops! Wrong format, fill the input please.';
-    CardDate1.style.borderColor = 'red';
     isValid = false;
-  }else{
-    CardDate1.style.borderColor = '';
   }
 
   if (!/^\d{3}$/.test(CardCVC.value)) {
-    CardDateError.textContent = 'Woops! Wrong format, fill the input please.';
-    CardCVC.style.borderColor = 'red';
     isValid = false;
-  }else{
-    CardCVC.style.borderColor = '';
   }
 
   if (!isValid) {
